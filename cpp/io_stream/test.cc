489aad7f644dc97361dfef01cf86d99af1bf85b4
live4qy <1404857825@qq.com>
2023-07-18 22:55:52 +0800

    cpp io

---
+++ cpp/io_stream/test.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+int main() {
+  int i;
+  while(std::cin>>i, !std::cin.eof()) {
+    if (std::cin.bad()) {
+      // 不可恢复的错误  
+      std::cout << "std::cin is bad." << std::endl;
+      break;
+    } else if (std::cin.fail()) {
+      // 格式错误
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "data format error, please try again" << std::endl;
+      continue;
+    }
+    std::cout << i << std::endl; 
+  }
+  std::cout << "process" << std::endl;
+  return 0;
+}
